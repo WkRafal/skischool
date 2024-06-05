d3127a83ce83ab36b660fecf142886115a4ca70a
WkRafal <wiezikrafal@gmail.com>
2024-06-05 10:20:03 +0200

    add login

--- routing.php
+++ routing.php
@@ -3,8 +3,10 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('hello'); #default action
-//App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
+App::getRouter()->setDefaultRoute('loginShow'); #default action
+App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('hello', 'HelloCtrl');
-//Utils::addRoute('action_name', 'controller_class_name');
+Utils::addRoute('loginShow',     'LoginCtrl');
+Utils::addRoute('login',         'LoginCtrl');
+Utils::addRoute('logout',        'LoginCtrl');
